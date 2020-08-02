90eb5a4ebffd27359743d875cfa7541abf99c053
Jan <kloppjp@gmail.com>
2020-08-02 18:41:05 +0800

    Find library automatically if provided in LD_LIBRARY_PATH

--- setup.py
+++ setup.py
@@ -2,18 +2,36 @@ from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import sys
 import numpy as np
+from typing import List, Union
+from pathlib import Path
+import os
+
+
+def find_containing_folder(folders: List[str], filename: str) -> Union[str]:
+    for f in folders:
+        if Path(f).joinpath(filename).is_file():
+            return f
+    return None
+
 
 library_dirs = list()
+found_libde265 = False
 for idx, argument in enumerate(sys.argv):
     if argument == "--libde265_path":
         library_dirs.append(sys.argv[idx+1])
         del sys.argv[idx+1]
         del sys.argv[idx]
+        found_libde265 = True
         break
     elif argument.startswith("--libde265_path="):
         library_dirs.append(argument.split('=')[1])
         del sys.argv[idx]
+        found_libde265 = True
         break
+if not found_libde265:
+    folder = find_containing_folder(os.environ.get('LD_LIBRARY_PATH').split(':'), "libde265.so")
+    if folder is not None:
+        library_dirs.append(folder)
 
 
 extensions = [Extension("pyde265.image", ['pyde265/image.pyx'], libraries=['de265'], include_dirs=['include', np.get_include()],
